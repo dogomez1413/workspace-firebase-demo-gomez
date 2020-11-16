fd62f089db865279f852688f5d1c1d0628dea495
dogomez1413 <dogomez@usca.edu>
2020-11-16 13:13:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# workspace-firebase-demo-gomez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/workspace-firebase-demo-gomez)
---
+++ js/SaveDisplay.js
@@ -0,0 +1,25 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// save the data
+$(".sampleSurvey input[type='submit']").click(function(e) {
+  e.preventDefault();
+
+  // get the value of the form using seralize array
+
+  var inputdata = $("form").serializeArray();
+  console.log(inputdata);
+  var inputJson = {};
+  for (var i = 0; i < inputdata.length; i++) {
+    var name = inputdata[i]["name"];
+    var value = inputdata[i]["value"];
+    inputJson[name] = value;
+  }
+
+  console.log(inputJson);
+});
+
+// update the result in table
